fff146d9d9a1081eb5353a63592a946854f19f0d
MarharytaPris <prismargo08@gmail.com>
2025-05-15 14:17:21 +0200

    WebSite programiert

--- Scripting/JavaScript/JavaSkriptAusprobieren.js
+++ Scripting/JavaScript/JavaSkriptAusprobieren.js
@@ -1,11 +1,15 @@
 const readline = require("readline-sync");
 
 class Person {
+    constructor(name, age) {
     this.name = name;
     this.age = age;
+    }
 }
 
 let name = readline.question("Wie heisst du? ");
-const user = new Person(name);
+
+let alter = readline.question("Wie alt bist du? ");
+const user = new Person(name, alter);
 console.log("Name: " + user.name);
 console.log("Alter: " + user.age);
